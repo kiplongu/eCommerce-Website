a0220e9f927717bf99a94d3ae7ad950e9d4a17fc
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-24 19:39:36 +0300

    rendered the category url using class

--- eCommerce/app/views.py
+++ eCommerce/app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+#render a page using a function
 def home(request):
     return render(request, "app/home.html")
+
+
+#render a page using a class
+class CategoryView(View):
+    def get(self, request, val):
+        return render(request, 'app/category.html', locals())
