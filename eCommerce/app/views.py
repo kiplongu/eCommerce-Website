2296ccecb3b38c65c4c94cbec54c565e4b375c1f
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-25 11:16:42 +0300

    Added CategoryTitle function and rendered the page at same category url

--- eCommerce/app/views.py
+++ eCommerce/app/views.py
@@ -19,6 +19,13 @@ class CategoryView(View):
         return render(request, 'app/category.html', locals())
 
 
+class CategoryTitle(View):
+    def get(self, request, val):
+        product = Product.objects.filter(title=val)
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, 'app/category.html', locals())
+
+
 class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
