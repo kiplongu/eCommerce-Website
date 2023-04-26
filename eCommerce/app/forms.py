f557fd268e7c805a51a3cdaf8976fabfd726aed4
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 09:22:57 +0300

    Added loginform function

--- eCommerce/app/forms.py
+++ eCommerce/app/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
+
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus': 'True', 'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class' : 'form-control'}))
 
 
 class CustomerRegistrationForm(UserCreationForm):
