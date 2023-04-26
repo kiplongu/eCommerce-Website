5c498cb16c541267ad602b991ae5de42b1568851
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 11:42:33 +0300

    Registered the Customer table model

--- eCommerce/app/admin.py
+++ eCommerce/app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from . models import Product
+from . models import Product, Customer
 
 # Register your models here.
 
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'locality', 'city', 'state', 'zipcode']
