f552cc603ea9be1b4c876bcf0cf1339d7ceea1aa
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 09:52:41 +0300

    Added class for reseting password

--- eCommerce/app/forms.py
+++ eCommerce/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
 from django.contrib.auth.models import User
 
 
@@ -18,3 +18,7 @@ class CustomerRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class MyPasswordResetForm(PasswordChangeForm):
+    pass
