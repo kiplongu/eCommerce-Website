5f06719eee80cf1e86a1ecac488d00eae9f91624
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 13:05:43 +0300

    Added customer profile form to capture customer's data

--- eCommerce/app/forms.py
+++ eCommerce/app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
 from django.contrib.auth.models import User
+from .models import Customer
 
 
 
@@ -22,3 +23,17 @@ class CustomerRegistrationForm(UserCreationForm):
 
 class MyPasswordResetForm(PasswordChangeForm):
     pass
+
+
+class CustomerProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name', 'locality', 'city', 'mobile', 'state', 'zipcode']
+        widgets = {
+            'name' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'locality' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'city' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'mobile' : forms.NumberInput(attrs={'class' : 'form-control'}),
+            'state' : forms.Select(attrs={'class' : 'form-control'}),
+            'zipcode' : forms.NumberInput(attrs={'class' : 'form-control'}),
+        }
