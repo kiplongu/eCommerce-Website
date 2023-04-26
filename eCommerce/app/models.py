0fa308082950ed828e25a8a98a495d966d1f34dc
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 11:41:36 +0300

    Created customer table model

--- eCommerce/app/models.py
+++ eCommerce/app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATE_CHOICES = (
+    ('Eldoret', 'Eldoret'),
+    ('Kisumu', 'Kisumu'),
+    ('Narok', 'Narok'),
+    ('Nairobi', 'Nairobi'),
+    ('Mombasa', 'Mombasa'),
+    ('Nakuru', 'Nakuru'),
+    ('Marakwet', 'Marakwet'),
+    ('Lodwar', 'Lodwar'),
+)
+
 
 CATEGORY_CHOICES = (
     ('CR', 'Curd'),
@@ -26,3 +39,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+
+    def __str__(self):
+        return self.name
+
