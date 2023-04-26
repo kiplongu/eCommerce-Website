e6f39f9433551b0aef0f3bb73076314b1d74b121
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-26 17:53:46 +0300

    Added the updateAddress url path

--- eCommerce/app/urls.py
+++ eCommerce/app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('product-detail/<int:pk>', views.ProductDetail.as_view(), name="product-detail"),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
+    path('updateAddress/<int:pk>', views.updateAddress.as_view(), name="updateAddress"),
 
     #login authentication
     path('registration/', views.CustomerRegistrationView.as_view(), name="customerregistration"),
