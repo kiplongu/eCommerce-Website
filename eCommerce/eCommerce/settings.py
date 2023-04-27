8afc93a91936d30bc4be0b349e2c03db0c775094
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-27 16:45:35 +0300

    Added email backend console- where password reset is sent in the console

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -125,3 +125,5 @@ LOGIN_REDIRECT_URL = '/profile/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
