e88726c7302f594b8debb2e59f59c0aa21c79529
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-27 11:39:33 +0300

    Added passwordchange url path

--- eCommerce/app/urls.py
+++ eCommerce/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from . forms import LoginForm, MyPasswordResetForm
+from . forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 
 urlpatterns = [
@@ -21,6 +21,9 @@ urlpatterns = [
     path('registration/', views.CustomerRegistrationView.as_view(), name="customerregistration"),
     path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name="login"),
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name="password_reset"),
+
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name="passwordchange"),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name="passwordchangedone"),
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
