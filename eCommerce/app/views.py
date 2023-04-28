df649566ad6d172d12386af4be252d4b38be6163
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-28 10:34:40 +0300

    Made the add-to-cart and show-cart views

--- eCommerce/app/views.py
+++ eCommerce/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from urllib import request
 from django.http import HttpResponse
 from django.views import View
-from . models import Product, Customer
+from . models import Product, Customer, Cart
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
@@ -109,5 +109,19 @@ class updateAddress(View):
         else:
             messages.warning(request, 'Invalid Input Data')
         return redirect('address')
+
+
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    return render(request, 'app/addtocart.html', locals())
+
 
 
