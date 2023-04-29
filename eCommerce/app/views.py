9dc87ca8dd339e0d33672fe69bd28f48bb514348
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-29 19:24:37 +0300

    Implemented  plus-cart function on the quantity of the product

--- eCommerce/app/views.py
+++ eCommerce/app/views.py
@@ -1,7 +1,7 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.shortcuts import render, redirect
 from urllib import request
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from . models import Product, Customer, Cart
 from . forms import CustomerRegistrationForm, CustomerProfileForm
@@ -127,6 +127,28 @@ def show_cart(request):
         amount = amount + value
     totalamount = amount + 40
     return render(request, 'app/addtocart.html', locals())
+
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity+=1
+        c.save()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 40
+        data={
+            'quantity':c.quantity,
+            'amount': amount,
+            'totalamount': totalamount
+
+        }
+        return JsonResponse(data)
 
 
 
