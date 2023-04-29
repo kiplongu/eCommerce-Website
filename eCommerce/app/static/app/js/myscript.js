106bbee5a49696b3affdf694c84aa3814c4addf5
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-29 19:27:11 +0300

    Implemented ajax method for plus-cart functionality

---
+++ eCommerce/app/static/app/js/myscript.js
@@ -0,0 +1,20 @@
+//Ajax is method for connecting with the server without refreshing the web page
+
+$('.plus-cart').click(function(){
+    var id = $(this).attr('pid').toString();
+    var eml = this.parentNode.children[2]
+    console.log('pid=', id)
+    $.ajax({
+        type :'GET',
+        url : '/pluscart',
+        data : {
+            prod_id : id
+        },
+        success:function(data){
+            console.log('data =', data);
+            eml.innerText=data.quantity
+            document.getElementById('amount').innerText=data.amount
+            document.getElementById('totalamount').innerText=data.totalamount
+        }
+    })
+})
