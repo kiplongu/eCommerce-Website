683ca5e0ec03ad69c58fa175cc61684641a50077
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-29 19:35:47 +0300

    Implemented ajax method for minus-cart functionality

--- eCommerce/app/static/app/js/myscript.js
+++ eCommerce/app/static/app/js/myscript.js
@@ -18,3 +18,24 @@ $('.plus-cart').click(function(){
         }
     })
 })
+
+
+
+$('.minus-cart').click(function(){
+    var id = $(this).attr('pid').toString();
+    var eml = this.parentNode.children[2]
+    console.log('pid=', id)
+    $.ajax({
+        type :'GET',
+        url : '/minuscart',
+        data : {
+            prod_id : id
+        },
+        success:function(data){
+            console.log('data =', data);
+            eml.innerText=data.quantity
+            document.getElementById('amount').innerText=data.amount
+            document.getElementById('totalamount').innerText=data.totalamount
+        }
+    })
+})
