f42e88a904ef08b2080dd807690c1756cd50ddac
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-04-29 19:56:27 +0300

    Implemented ajax method for remove-cart functionality

--- eCommerce/app/static/app/js/myscript.js
+++ eCommerce/app/static/app/js/myscript.js
@@ -24,7 +24,6 @@ $('.plus-cart').click(function(){
 $('.minus-cart').click(function(){
     var id = $(this).attr('pid').toString();
     var eml = this.parentNode.children[2]
-    console.log('pid=', id)
     $.ajax({
         type :'GET',
         url : '/minuscart',
@@ -32,10 +31,28 @@ $('.minus-cart').click(function(){
             prod_id : id
         },
         success:function(data){
-            console.log('data =', data);
             eml.innerText=data.quantity
             document.getElementById('amount').innerText=data.amount
             document.getElementById('totalamount').innerText=data.totalamount
+        }
+    })
+})
+
+
+
+$('.remove-cart').click(function(){
+    var id = $(this).attr('pid').toString();
+    var eml = this
+    $.ajax({
+        type :'GET',
+        url : '/removecart',
+        data : {
+            prod_id : id
+        },
+        success:function(data){
+            document.getElementById('amount').innerText=data.amount
+            document.getElementById('totalamount').innerText=data.totalamount
+            eml.parentNode.parentNode.parentNode.parentNode.remove()
         }
     })
 })
